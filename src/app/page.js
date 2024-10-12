34a35a6774504f5204ce1357ad36858b65f121a4
Matheus Lima <matheus22lima2005@gmail.com>
2024-10-12 17:00:32 -0300

    caroseu pronto

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import styles from "./styles/page.module.css";
 import MainCarroseu from "./components/MainCarroseu";
-import { evento1,evento2,evento3,evento4 } from "../../public/images/imagesHome";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import customStyles from './styles/custon.module.css'
+
 
 export default function Home() {
   return (
@@ -10,49 +10,12 @@ export default function Home() {
     <div className={styles}>
       <div className="homeBody">
         <div className="home">
-
-          <div className="main-banner">
-
-          </div>
-          <div className="show-events-carousel">
-            <div className="container">
-              <MainCarroseu>
-              </MainCarroseu>
-
-
-            
-
-              {/* <div className="row">
-                <div className="col-lg-12">
-                  <div className="owl-show-events owl-carousel owl-loaded owl-drag">
-                    <div className="item">
-                      <a href=""><Image src={evento2} alt="" /></a>
-                    </div>
-                    <div className="item">
-                      <a href=""><Image src={evento1} alt="" /></a>
-                    </div>
-                    <div className="item">
-                      <a href=""><Image src={evento3} alt="" /></a>
-                    </div>
-                    <div className="item">
-                      <a href=""><Image src={evento2} alt="" /></a>
-                    </div>
-                    <div className="item">
-                      <a href=""><Image src={evento4} alt="" /></a>
-                    </div>
-                    <div className="item">
-                      <a href=""><Image src={evento1} alt="" /></a>
-                    </div>
-                    <div className="item">
-                      <a href=""><Image src={evento3} alt="" /></a>
-                    </div>
-                    <div className="item">
-                      <a href=""><Image src={evento4} alt="" /></a>
-                    </div>
-                  </div>
+            <div className="main-banner"></div>
+              <div className={customStyles.showEventsCarousel} >
+                <div className="container" >
+                  <MainCarroseu/>                
                 </div>
-              </div> */}
-            </div>
+              </div>
           </div>
           <div className="conheca">
             <div className="container">
@@ -62,7 +25,6 @@ export default function Home() {
                     <h4>Conheça a comunidade Tabor:</h4>
                     <p>O Taborão é um evento anual promovido pela Arquidiocese de Cascavel, que se destaca como uma celebração de fé e comunidade para jovens de todo o Brasil. Este evento, que tem ganhado crescente relevância e notoriedade, tem como objetivo principal proporcionar uma experiência espiritual rica e contemporânea, capaz de conectar a juventude ao ensinamento e à vivência cristã de maneira atual e envolvente.</p>
                   </div>
-                </div>
                 <div className="col-lg-3">
                   <div className="right-content">
                     <h5><i className="fa fa-map-marker"></i> Visite-Nos</h5>
